87697244fd53edc4bdd230b4522d08a329e41d9c
Swat IT SAS <swatit@argo-swatit0034.(none)>
2014-06-11 12:06:12 -0500

    Primer comit practica

---
+++ rails_practica/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsPractica::Application.config.secret_key_base = '4d93fcf9dd7abbec828a88d640b37b0117b6aaff03ee251d1500f789972c68895108c44b9c0e71b7da7bd9c21c4dae3ccbec6444e0306ee4a7c41999bf93f1ae'
