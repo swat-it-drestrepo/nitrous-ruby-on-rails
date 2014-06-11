6e758e4a1c8e1ba68fb266888c4bf810a950f7ee
Swat IT SAS <swatit@argo-swatit0034.(none)>
2014-06-11 16:46:37 -0500

    Termino practica 1 rails

--- rails_practica/config/routes.rb
+++ rails_practica/config/routes.rb
@@ -1,4 +1,10 @@
 RailsPractica::Application.routes.draw do
+  resources :microposts
+
+  resources :users
+  get '/nuevo' => 'users#new', as: 'new'
+  root 'users#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
